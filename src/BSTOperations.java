cc139b5465cebd0af7d2c256798e47e7f3aecb17
Arpit <arpit.porwal@skilrock.com>
2018-02-06 16:54:52 +0530

    abc2

--- src/BSTOperations.java
+++ src/BSTOperations.java
@@ -117,6 +117,54 @@ public class BSTOperations {
 		}
 	}
 	
+	//function to print nodes at K distance from root
+		public static void printKNodesDown(BSTNode root,int k){
+			if(root ==null || k<0)
+				return;
+			if(k==0)
+				System.out.print(root.data);
+			else{
+				printKNodesDown(root.left, k-1);
+				printKNodesDown(root.right, k-1);
+			}
+		}
+		
+		public static int printNodesAtKDistance(BSTNode root,BSTNode target,int k){
+			if(root==null)
+				return -1;
+			if(root.data==target.data){
+				printKNodesDown(root, k);
+				return 0;
+			}
+			//dl is distance between roots left child and target node
+			int dl = printNodesAtKDistance(root.left, target, k);
+			//if target found in left subtree
+			if(dl!=-1){
+				//check if distance +1 -> root to target distance is k then print root data
+				//else go to right child of root and reduce the distance by k-dl-2 as distance between
+				//roots left child and right child is 2
+				if(dl+1==k)
+					System.out.print(root.data);
+				else
+					printKNodesDown(root.right, k-dl-2);
+				
+				return dl+1;
+			}
+			
+			//same as above for right subtree
+			int dr = printNodesAtKDistance(root.right, target, k);
+			if(dr!=-1){
+				if(dr+1==k)
+					System.out.print(root.data);
+				else
+					printKNodesDown(root.left, k-dr-2);
+				
+				return dr+1;
+			}
+			
+			return -1;
+		}
+	
 	public static void main(String[] args) {
 			//BinarySearchTree
 			BSTNode bstNode = BSTNode.insert(null, 7);
@@ -140,6 +188,10 @@ public class BSTOperations {
 			System.out.println(count);
 			int[] arr = new int[100];
 			rootToLeafPath(bstNode, arr, 0);
+			System.out.println();
+			BSTNode newNode = new BSTNode();
+			newNode.data = 8;
+			printNodesAtKDistance(bstNode, newNode, 1);
 	}
 
 }
--- src/TreeOperations.java
+++ src/TreeOperations.java
@@ -89,6 +89,53 @@ public class TreeOperations {
 		}
 	}
 	
+	//function to print nodes at K distance from root
+	public static void printKNodesDown(Node root,int k){
+		if(root ==null || k<0)
+			return;
+		if(k==0)
+			System.out.print(root.data);
+		else{
+			printKNodesDown(root.left, k-1);
+			printKNodesDown(root.right, k-1);
+		}
+	}
+	
+	public static int printNodesAtKDistance(Node root,Node target,int k){
+		if(root==null)
+			return -1;
+		if(root.data==target.data){
+			printKNodesDown(root, k);
+			return 0;
+		}
+		//dl is distance between roots left child and target node
+		int dl = printNodesAtKDistance(root.left, target, k);
+		//if target found in left subtree
+		if(dl!=-1){
+			//check if distance +1 -> root to target distance is k then print root data
+			//else go to right child of root and reduce the distance by k-dl-2 as distance between
+			//roots left child and right child is 2
+			if(dl+1==k)
+				System.out.print(root.data);
+			else
+				printKNodesDown(root.right, k-dl-2);
+			
+			return dl+1;
+		}
+		
+		//same as above for right subtree
+		int dr = printNodesAtKDistance(root.right, target, k);
+		if(dr!=-1){
+			if(dr+1==k)
+				System.out.print(root.data);
+			else
+				printKNodesDown(root.left, k-dr-2);
+			
+			return dr+1;
+		}
+		
+		return -1;
+	}
 	
 	public static void main(String[] args) {
 		
@@ -114,6 +161,10 @@ public class TreeOperations {
 		System.out.println();
 		int[] arr = new int[100];
 		rootToLeafPath(root, arr, 0);
+		System.out.println();
+		Node newNode = new Node();
+		newNode.data = 8;
+		printNodesAtKDistance(root, newNode, 1);
 	}
 	
 }
